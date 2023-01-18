f3e7de2e02812b2cc54e3e72fdbd852f6ab39d91
Samar <samar.abdelhamed95@gmail.com>
2023-01-18 04:18:27 +0400

    404 page

---
+++ src/views/Error.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="error light_grey_bg">
+    <div class="container py-5">
+      <div class="row">
+        <div class="col-9 mx-auto">
+          <div
+            class="white_box p-5 d-flex flex-column justify-content-center align-items-center"
+          >
+            <img
+              src="@/assets/images/error.png"
+              alt=".."
+              class="img_fluid error_page"
+            />
+            <h2 class="black_font capitalize mt-3">page not found !</h2>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<style scoped>
+.error_page {
+  width: 600px;
+}
+</style>

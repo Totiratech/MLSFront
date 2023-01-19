892a1e97b4e3894fc6fe280d9520c967b14edd66
Samar <samar.abdelhamed95@gmail.com>
2023-01-19 04:29:04 +0400

    search-style

--- src/components/global/AppHeader.vue
+++ src/components/global/AppHeader.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <div class="row">
         <div class="col-12">
-          <nav class="navbar navbar-expand-lg bg-light">
+          <nav class="navbar navbar-expand-lg">
             <div class="container-fluid">
               <a class="navbar-brand" href="#">
                 <img src="@/assets/images/navLogo.png" class="img-fluid" />

ae10304e07a7db0c311ba5498c37136dba85965b
Samar <samar.abdelhamed95@gmail.com>
2023-01-24 02:39:17 +0400

    display-img

--- src/components/HomeDetailCard.vue
+++ src/components/HomeDetailCard.vue
@@ -1,76 +1,84 @@
 <template>
   <div class="card" v-if="home">
-    <!-- {{ home }} -->
-    <img
-      src="@/assets/images/staticHome.png"
-      class="card-img-top img-fluid"
-      alt="..."
-    />
-    <div class="card-body pe-0">
-      <div class="row pt-2">
-        <div class="col-6">
-          <span class="main_color price">$ {{ home.Orig_dol }}</span>
-        </div>
-        <div class="col-6 d-flex align-items-center">
-          <font-awesome-icon
-            icon="fa-solid fa-heart"
-            class="pe-2 fav_icon"
-            @click.prevent="favouriteAction()"
-          />
-          <div
-            class="rent_bg text-center"
-            v-if="home.S_r == 'rent' || home.S_r == 'lease'"
-          >
-            <span>Rent</span>
+    <!-- {{ home.Ml_num }} -->
+    <router-link :to="`/find?type=${home.S_r}?ml_num=${home.Ml_num}`">
+      <!-- {{ home }} -->
+      <!-- src="@/assets/images/staticHome.png" -->
+      <div class="img_container">
+        <img
+          :src="img_url + '/' + home.Ml_num + '/0.jpg'"
+          class="card-img-top img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="card-body pe-0">
+        <div class="row pt-2">
+          <div class="col-6">
+            <span class="main_color price">$ {{ home.Orig_dol }}</span>
           </div>
-          <div class="sale_bg text-center" v-if="home.S_r == 'sale'">
-            <span>{{ home.S_r }}</span>
-          </div>
-        </div>
-        <div class="col-12 py-2 text-start">
-          <div class="d-flex align-items-start black_font">
-            <img
-              src="@/assets/images/mapMarker.png"
-              alt=".."
-              class="img-fluid pt-1 pe-2"
+          <div class="col-6 d-flex align-items-center">
+            <font-awesome-icon
+              icon="fa-solid fa-heart"
+              class="pe-2 fav_icon"
+              @click.prevent="favouriteAction()"
             />
-            <span class="small_font">
-              {{
-                home.Addr +
-                ", " +
-                home.Municipality +
-                ", " +
-                home.County +
-                ", " +
-                home.Zip
-              }}
-            </span>
+            <div
+              class="rent_bg text-center"
+              v-if="home.S_r == 'rent' || home.S_r == 'sale'"
+            >
+              <span>Rent</span>
+            </div>
+            <div class="sale_bg text-center" v-else>
+              <span>{{ home.S_r }}</span>
+            </div>
           </div>
-        </div>
-        <div class="col-4">
-          <div class="d-flex align-items-center">
-            <img src="@/assets/images/bed.png" alt="..." class="img-fluid" />
-            <span class="small_font capitalize">{{ home.Br }} beds</span>
+          <div class="col-12 py-2 text-start">
+            <div class="d-flex align-items-start black_font">
+              <img
+                src="@/assets/images/mapMarker.png"
+                alt=".."
+                class="img-fluid pt-1 pe-2"
+              />
+              <span class="small_font">
+                {{
+                  home.Addr +
+                  ", " +
+                  home.Municipality +
+                  ", " +
+                  home.County +
+                  ", " +
+                  home.Zip
+                }}
+              </span>
+            </div>
           </div>
-        </div>
-        <div class="col-4">
-          <div class="d-flex align-items-center">
-            <img src="@/assets/images/dis.png" alt="..." class="img-fluid" />
-            <span class="small_font capitalize">{{ homeDistance }} ft2</span>
+          <div class="col-4">
+            <div class="d-flex align-items-center">
+              <img src="@/assets/images/bed.png" alt="..." class="img-fluid" />
+              <span class="small_font capitalize">{{ home.Br }} beds</span>
+            </div>
           </div>
-        </div>
-        <div class="col-4">
-          <div class="d-flex align-items-center">
-            <img
-              src="@/assets/images/bath.png"
-              alt="..."
-              class="img-fluid pe-1"
-            />
-            <span class="small_font capitalize">{{ home.Bath_tot }} baths</span>
+          <div class="col-4">
+            <div class="d-flex align-items-center">
+              <img src="@/assets/images/dis.png" alt="..." class="img-fluid" />
+              <span class="small_font capitalize">{{ homeDistance }} ft2</span>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="d-flex align-items-center">
+              <img
+                src="@/assets/images/bath.png"
+                alt="..."
+                class="img-fluid pe-1"
+              />
+              <span class="small_font capitalize"
+                >{{ home.Bath_tot }} baths</span
+              >
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </router-link>
   </div>
 </template>
 <script>
@@ -83,6 +91,8 @@ export default {
     return {
       userID: "",
       option: "remove",
+      ml_num: this.home.ML_num,
+      img_url: "https://totira2.crimsonrose.a2hosted.com/images/",
     };
   },
   computed: {
@@ -148,7 +158,7 @@ export default {
 }
 .price {
   font-weight: 600;
-  font-size: 1.3em;
+  font-size: 1.2em;
   font-family: "Lato-Regular";
 }
 .card {
@@ -159,5 +169,17 @@ export default {
 }
 .fa-heart {
   cursor: pointer;
+}
+a,
+a:hover {
+  color: unset;
+  text-decoration: none;
+}
+.img_container {
+  width: 100%;
+  height: 200px;
+}
+.img-container img {
+  height: 100%;
 }
 </style>

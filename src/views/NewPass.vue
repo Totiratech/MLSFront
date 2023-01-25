0fc948ccc6de9ec048a9a45a38310016da403cf2
enas404 <enasamir787@gmail.com>
2023-01-25 21:18:05 +0200

    search animation & some fix

--- src/views/NewPass.vue
+++ src/views/NewPass.vue
@@ -13,7 +13,7 @@
                 >
                 <input type="password" class="form-control" id="password" />
                 <span
-                  toggle="password"
+                  toggle="#password"
                   class="fa fa-fw fa-eye field-icon toggle-password"></span>
               </div>
               <div class="mt-4">
